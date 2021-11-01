cb29be1f5669461a3fd057000e7275cf64163a8e
Endia Clark <78235607+rhit-clarken@users.noreply.github.com>
2021-11-01 14:52:04 -0400

    worked on login page, animation, buttons do not work

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -27,10 +27,46 @@ rhit.ClassName = class {
 	}
 }
 
+rhit.LoginPageController = class {
+	constructor() {
+
+		document.querySelector("#indexBody").onclick = (event) => {
+			document.querySelector("#indexBody").classList.add("bodyIntroAnimation");
+			document.querySelector("#loginThe100").classList.add("textIntroAnimation")
+			// document.querySelector("#firebaseui-auth-container").style.display = "block";
+			// document.querySelector("#rosefireButton").style.display = "block";
+			document.querySelector("#firebaseui-auth-container").style.opacity = "1";
+			document.querySelector("#rosefireButton").style.opacity = "1";
+		}
+
+		// document.querySelector("#rosefireButton").onclick = (event) => {
+		// 	rhit.fbAuthManager.signIn();
+		// };
+	}
+}
+
+
+rhit.startFirebaseUI = function() {
+	var uiConfig = {
+		signInSuccessUrl: '/bucket.html',
+		signInOptions: [
+		  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+		  firebase.auth.EmailAuthProvider.PROVIDER_ID,
+		  firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+		  firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+		],
+	  };
+
+	  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+	  ui.start('#firebaseui-auth-container', uiConfig);
+}
+
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	new rhit.LoginPageController();
+	rhit.startFirebaseUI();
 };
 
 rhit.main();
